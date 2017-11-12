0b663f09b6a0140f0a04053ad8a0875f148cd1b9
HorizonP <horizonp@Peiyus-MacBook-Air.local>
2017-11-11 21:57:48 -0500

    make it robust

--- Analysis/wrapper_handle_WNdata.m
+++ Analysis/wrapper_handle_WNdata.m
@@ -1,21 +1,29 @@
-function recep_handle=wrapper_handle_WNdata(resD,stimD,detrend_level,sign)
+function recep_handle=wrapper_handle_WNdata(D1,D2,detrend_level,sign)
+if ~endsWith(D1,'.mat')
+    D1=[D1 '.mat'];
+end
+if ~endsWith(D2,'.mat')
+    D2=[D2 '.mat'];
+end
+try
+    stimD=D1;
+    resD=D2;
+    load(stimD,'mosaicMat','ActualRect','mosaicSzInReal');
+    load(resD,'data','datastart','dataend','tickrate');
+    chan5_TTL=data(datastart(5):dataend(5));
+catch err
+    stimD=D2;
+    resD=D1;
+    load(stimD,'mosaicMat','ActualRect','mosaicSzInReal');
+    load(resD,'data','datastart','dataend','tickrate');
+    chan5_TTL=data(datastart(5):dataend(5));
+end
 name=resD(1:end-4);
-if ~endsWith(resD,'.mat')
-    name=resD;
-    resD=[resD '.mat'];
 
-end
-if ~endsWith(stimD,'.mat')
-    resD=[stimD '.mat'];
-end
-
-load(stimD,'mosaicMat','ActualRect','mosaicSzInReal');
-
-load(resD,'data','datastart','dataend','tickrate');
 %necessary labchart data preprocessing for whitenoise with grey at two ends
 %range for analysis is from the onset of 1st TTL to offset to last TTL
 
-chan5_TTL=data(datastart(5):dataend(5));
+
 chan5_TTL(chan5_TTL<0.5)=0;
 chan5_TTL(chan5_TTL>=0.5)=1;
 
@@ -28,7 +36,7 @@ chan2=detrend(chan2)*10e12;%pA
 
 clear data;
 
-response=sign*response;
+chan2=sign*chan2;
 T=(0:length(chan2)-1)/tickrate; %T start from 0
 %find all the ascending edge off TTL (indices)
 ascendT=find([1 chan5(1:end-1)]==0&chan5==1); %skipped the first TTL block
